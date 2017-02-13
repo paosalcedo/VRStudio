a43cbdf5d175d3f4766527fc24eec6d8feab425c
Unknown <paosalcedo@gmail.com>
2017-02-13 18:12:38 -0500

    Added VR Project 3

---
+++ VR Project 3/Assets/Scripts/NativeVRController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR; 
+
+public class NativeVRController : MonoBehaviour {
+	[SerializeField] Transform leftHand, rightHand;
+
+
+	void Update () {
+		leftHand.localPosition = InputTracking.GetLocalPosition (VRNode.LeftHand);
+		leftHand.localRotation = InputTracking.GetLocalRotation (VRNode.LeftHand);
+		rightHand.localPosition = InputTracking.GetLocalPosition (VRNode.RightHand);
+		rightHand.localRotation = InputTracking.GetLocalRotation (VRNode.RightHand);
+	}
+}
